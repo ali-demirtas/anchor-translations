11e85f3f9533a3a37db88dd0af820ed7ffdee015
Ali Demirtas <alidemirtas94@gmail.com>
2017-10-25 23:43:01 +0300

    Update comments.php

--- tr_TR/comments.php
+++ tr_TR/comments.php
@@ -3,33 +3,33 @@
 return array(
 
 	'comments' => 'Yorumlar',
-	'nocomments_desc' => 'Hen�z yorum yok.',
-	'editing_comment' => 'Yorum d�zenleme',
-	'view_comment' => 'Yorum g�r�nt�le',
+	'nocomments_desc' => 'Henüz yorum yok.',
+	'editing_comment' => 'Yorum düzenleme',
+	'view_comment' => 'Yorumu görüntüle',
 
 	// form fields
 	'name' => 'Name',
-	'name_explain' => 'Yazar ad�',
-	'name_missing' => 'L�tfen isim girin',
+	'name_explain' => 'Yazar adı',
+	'name_missing' => 'Lütfen isim girin',
 
 	'email' => 'Eposta adresi',
 	'email_explain' => 'Yazar epostas�',
-	'email_missing' => 'Ge�erli bir eposta adresi girin', // frontend message (appears on your site!)
+	'email_missing' => 'Geçerli bir e-posta adresi girin', // frontend message (appears on your site!)
 
 	'text' => 'Yorum',
 	'text_explain' => '',
-	'text_missing' => 'Yorum yaz�s� girin', // frontend message (appears on your site!)
+	'text_missing' => 'Yorum girin', // frontend message (appears on your site!)
 
 	'status' => 'Durum',
 	'status_explain' => '',
 
 	// messages
 	'created' => 'Yorumunuz eklendi', // frontend message (appears on your site!)
-	'updated' => 'Yorumunuz g�ncellendi',
+	'updated' => 'Yorumunuz güncellendi',
 	'deleted' => 'Yorumunuz silindi',
 
 	// email notification
 	'notify_subject' => 'Yeni yorum eklendi',
-	'nofity_heading' => 'Sitenize yeni bir yorum g�nderildi.'
+	'nofity_heading' => 'Sitenize yeni bir yorum gönderildi.'
 
 );
